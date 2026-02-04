061e636af1461e234fc99c7f2922aa09b3b24a2f
daffaghiffarykusuma <daffaghifarykusuma@gmail.com>
2026-02-05 03:09:54 +0700

    build

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Provides offline caching and data persistence
  */
 
-const CACHE_VERSION = 'v1770218528808-68f207b';
+const CACHE_VERSION = 'v1770235762535-e76ebc8';
 const STATIC_CACHE = `rekonime-static-${CACHE_VERSION}`;
 const DATA_CACHE = `rekonime-data-${CACHE_VERSION}`;
 const IMAGE_CACHE = `rekonime-images-${CACHE_VERSION}`;
