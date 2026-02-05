2a0b6dc3ec3181471a27d677efcef2c4e11da481
daffaghiffarykusuma <daffaghifarykusuma@gmail.com>
2026-02-05 18:24:04 +0700

    updated cache

--- dist/sw.js
+++ dist/sw.js
@@ -3,7 +3,7 @@
  * Provides offline caching and data persistence
  */
 
-const CACHE_VERSION = 'v1770282444354-ff047d4';
+const CACHE_VERSION = 'v1770290619040-23bc6b1';
 const STATIC_CACHE = `rekonime-static-${CACHE_VERSION}`;
 const DATA_CACHE = `rekonime-data-${CACHE_VERSION}`;
 const IMAGE_CACHE = `rekonime-images-${CACHE_VERSION}`;
